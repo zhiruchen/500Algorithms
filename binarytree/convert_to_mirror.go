c22b0305115119c5122eeda94dc19b8f4337d70e
zhiruchen <zhiruchen1992@foxmail.com>
2018-08-01 21:50:41 +0800

    Binary tree Convert to Mirror

--- binarytree/convert_to_mirror.go
+++ binarytree/convert_to_mirror.go
@@ -1,1 +1,17 @@
 package binarytree
+
+import (
+	"github.com/zhiruchen/500Algorithms/types"
+)
+
+// ConvertToMirror convert binary tree to it's mirror
+func ConvertToMirror(root *types.BinTreeNode) {
+	if root == nil {
+		return
+	}
+
+	ConvertToMirror(root.Left)
+	ConvertToMirror(root.Right)
+
+	root.Left, root.Right = root.Right, root.Left
+}
