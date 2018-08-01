7f2e7c9b40b78983945c4a2a64a70b7cf4e6e61d
zhiruchen <zhiruchen1992@foxmail.com>
2018-08-01 08:35:26 +0800

    Binary Tree LCA

--- binarytree/lowest_common_ancestor.go
+++ binarytree/lowest_common_ancestor.go
@@ -1,1 +1,30 @@
 package binarytree
+
+import (
+	"github.com/zhiruchen/500Algorithms/types"
+)
+
+// LowestCommonAncestor lowest node in binary tree that has both x and y as descendants
+// http://www.techiedelight.com/find-lowest-common-ancestor-lca-two-nodes-binary-tree/
+func LowestCommonAncestor(root, x, y *types.BinTreeNode) *types.BinTreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Data == x.Data || root.Data == y.Data {
+		return root
+	}
+
+	left := LowestCommonAncestor(root.Left, x, y)
+	right := LowestCommonAncestor(root.Right, x, y)
+
+	if left != nil && right != nil {
+		return root
+	}
+
+	if left != nil {
+		return left
+	}
+
+	return right
+}
