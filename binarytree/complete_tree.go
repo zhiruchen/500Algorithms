5f1b8834f33201e5b802b2295526330784af0cd5
zhiruchen <zhiruchen1992@foxmail.com>
2018-08-02 08:53:23 +0800

    Binary tree: complete; find max path

--- binarytree/complete_tree.go
+++ binarytree/complete_tree.go
@@ -1,1 +1,48 @@
 package binarytree
+
+import (
+	list "github.com/emirpasic/gods/lists/arraylist"
+
+	"github.com/zhiruchen/500Algorithms/types"
+)
+
+// IsComplete every level except last is completely filled
+// http://www.techiedelight.com/check-given-binary-tree-complete-binary-tree-not/
+func IsComplete(root *types.BinTreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	queue := list.New()
+	queue.Add(root)
+
+	var isNonFull bool
+	for !queue.Empty() {
+		v, _ := queue.Get(0)
+		front := v.(*types.BinTreeNode)
+		queue.Remove(0)
+
+		// 前面的遍历过程中遇到了不完全的节点，而且当前节点不是叶子节点，则二叉树不完全
+		if isNonFull && (front.Left != nil || front.Right != nil) {
+			return false
+		}
+
+		if front.Left == nil && front.Right != nil {
+			return false
+		}
+
+		if front.Left != nil {
+			queue.Add(front.Left)
+		} else {
+			isNonFull = true
+		}
+
+		if front.Right != nil {
+			queue.Add(front.Right)
+		} else {
+			isNonFull = true
+		}
+	}
+
+	return true
+}
--- binarytree/find_maxsum_path.go
+++ binarytree/find_maxsum_path.go
@@ -1,1 +1,47 @@
 package binarytree
+
+import (
+	"fmt"
+
+	"github.com/zhiruchen/500Algorithms/types"
+)
+
+func rootToLeafSum(root *types.BinTreeNode) int32 {
+	if root == nil {
+		return 0
+	}
+
+	leftSum := rootToLeafSum(root.Left)
+	rightSum := rootToLeafSum(root.Right)
+
+	if leftSum >= rightSum {
+		return leftSum + root.Data
+	}
+
+	return rightSum + root.Data
+}
+
+func printPath(root *types.BinTreeNode, sum int32) bool {
+	if root == nil {
+		return false
+	}
+
+	if sum == 0 {
+		return true
+	}
+
+	left := printPath(root.Left, sum-root.Data)
+	right := printPath(root.Right, sum-root.Data)
+
+	if left || right {
+		fmt.Println(root.Data, " ")
+	}
+
+	return left || right
+}
+
+// FindMaxSumPath find max sum path root to leaf
+func FindMaxSumPath(root *types.BinTreeNode) {
+	sum := rootToLeafSum(root)
+	printPath(root, sum)
+}
