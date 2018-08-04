6ccbe099c787f546e5fbdaac0a2bbd24a52aa6fc
zhiruchen <zhiruchen1992@foxmail.com>
2018-08-04 09:41:30 +0800

    Count number occurrences in sorted array

--- all/divideconquer/count_occurrences_of_a_number_in_a_sorted_array_with_duplicates.go
+++ all/divideconquer/count_occurrences_of_a_number_in_a_sorted_array_with_duplicates.go
@@ -1,7 +1,37 @@
 package divideconquer
 
 // CountOccurrencesOfANumberInASortedArrayWithDuplicates http://www.techiedelight.com/count-occurrences-number-sorted-array-duplicates/
-func CountOccurrencesOfANumberInASortedArrayWithDuplicates() {
+// 在一个排序数组中指定的数字出现的次数
+func CountOccurrencesOfANumberInASortedArrayWithDuplicates(array []int32, val int32) int32 {
+	first := binarySearch(array, val, true)
+	last := binarySearch(array, val, false)
+	if first == -1 {
+		return -1
+	}
 
+	return int32(last-first) + 1
 }
 
+func binarySearch(array []int32, val int32, searchFirst bool) int {
+	low, high := 0, len(array)-1
+	var result = -1
+
+	for low <= high {
+		mid := low + (high-low)/2
+
+		if array[mid] < val {
+			low = mid + 1
+		} else if array[mid] > val {
+			high = mid - 1
+		} else {
+			result = mid
+			if searchFirst {
+				high = mid - 1 // 如果搜索第一次出现的位置， 则在左边搜索
+			} else {
+				low = mid + 1 // 否则在👉搜索
+			}
+		}
+	}
+
+	return result
+}
--- all/divideconquer/count_occurrences_of_a_number_in_a_sorted_array_with_duplicates_test.go
+++ all/divideconquer/count_occurrences_of_a_number_in_a_sorted_array_with_duplicates_test.go
@@ -5,6 +5,37 @@ import (
 )
 
 func TestCountOccurrencesOfANumberInASortedArrayWithDuplicates(t *testing.T) {
-	
-}
+	cases := []struct {
+		input []int32
+		val   int32
+		count int32
+	}{
+		{
+			input: []int32{1, 2, 6, 8, 9, 100},
+			val:   9,
+			count: 1,
+		},
+		{
+			input: []int32{1, 2, 6, 8, 9, 9, 9, 100},
+			val:   9,
+			count: 3,
+		},
+		{
+			input: []int32{1, 1, 1, 2, 6, 8, 9, 100},
+			val:   1,
+			count: 3,
+		},
+		{
+			input: []int32{1, 2, 6, 8, 9, 100},
+			val:   -1,
+			count: -1,
+		},
+	}
 
+	for _, c := range cases {
+		count := CountOccurrencesOfANumberInASortedArrayWithDuplicates(c.input, c.val)
+		if count != c.count {
+			t.Errorf("Expected %d, get: %d\n", c.count, count)
+		}
+	}
+}
