b846332a2cabbc1afbcb966acf01bc7325bd34b7
zhiruchen <zhiruchen1992@foxmail.com>
2018-08-04 09:11:25 +0800

    Binary Search

--- all/divideconquer/binary_search.go
+++ all/divideconquer/binary_search.go
@@ -1,7 +1,20 @@
 package divideconquer
 
 // BinarySearch http://www.techiedelight.com/binary-search/
-func BinarySearch() {
+func BinarySearch(array []int32, val int32) int {
+	low, high := 0, len(array)-1
 
+	for low <= high {
+		mid := low + (high-low)/2
+
+		if array[mid] < val {
+			low = mid + 1
+		} else if array[mid] > val {
+			high = mid - 1
+		} else {
+			return mid
+		}
+	}
+
+	return -1
 }
-
--- all/divideconquer/binary_search_test.go
+++ all/divideconquer/binary_search_test.go
@@ -5,6 +5,37 @@ import (
 )
 
 func TestBinarySearch(t *testing.T) {
-	
-}
+	cases := []struct {
+		input []int32
+		val   int32
+		index int
+	}{
+		{
+			input: []int32{1, 2, 6, 8, 9, 100},
+			val:   9,
+			index: 4,
+		},
+		{
+			input: []int32{1, 2, 6, 8, 9, 100},
+			val:   1,
+			index: 0,
+		},
+		{
+			input: []int32{1, 2, 6, 8, 9, 100},
+			val:   100,
+			index: 5,
+		},
+		{
+			input: []int32{1, 2, 6, 8, 9, 100},
+			val:   -1,
+			index: -1,
+		},
+	}
 
+	for _, c := range cases {
+		index := BinarySearch(c.input, c.val)
+		if index != c.index {
+			t.Errorf("Expected %d, get: %d\n", c.index, index)
+		}
+	}
+}
