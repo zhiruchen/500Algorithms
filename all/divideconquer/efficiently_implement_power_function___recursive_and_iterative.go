892d06a5f732b63e47405d27512c3fdba1ce7b93
zhiruchen <zhiruchen1992@foxmail.com>
2018-08-04 10:04:58 +0800

    Efficient power

--- all/divideconquer/efficiently_implement_power_function___recursive_and_iterative.go
+++ all/divideconquer/efficiently_implement_power_function___recursive_and_iterative.go
@@ -1,7 +1,19 @@
 package divideconquer
 
 // EfficientlyImplementPowerFunctionRecursiveAndIterative http://www.techiedelight.com/power-function-implementation-recursive-iterative/
-func EfficientlyImplementPowerFunctionRecursiveAndIterative() {
-
+func EfficientlyImplementPowerFunctionRecursiveAndIterative(x int, n uint) int64 {
+	return power(x, n)
 }
 
+func power(x int, n uint) int64 {
+	if n == 0 {
+		return 1
+	}
+
+	pow := power(x, n/2)
+	if n%2 != 0 {
+		return int64(x) * pow * pow
+	}
+
+	return pow * pow
+}
--- all/divideconquer/efficiently_implement_power_function___recursive_and_iterative_test.go
+++ all/divideconquer/efficiently_implement_power_function___recursive_and_iterative_test.go
@@ -5,6 +5,37 @@ import (
 )
 
 func TestEfficientlyImplementPowerFunctionRecursiveAndIterative(t *testing.T) {
-	
-}
+	cases := []struct {
+		x     int
+		n     uint
+		power int64
+	}{
+		{
+			x:     2,
+			n:     3,
+			power: 8,
+		},
+		{
+			x:     2,
+			n:     4,
+			power: 16,
+		},
+		{
+			x:     -2,
+			n:     3,
+			power: -8,
+		},
+		{
+			x:     -2,
+			n:     10,
+			power: 1024,
+		},
+	}
 
+	for _, c := range cases {
+		power := EfficientlyImplementPowerFunctionRecursiveAndIterative(c.x, c.n)
+		if power != c.power {
+			t.Errorf("Expect: %d, get: %d\n", c.power, power)
+		}
+	}
+}
