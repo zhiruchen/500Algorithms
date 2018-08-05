1596913781cb304549f9dae7bdd5c147c90f48bb
zhiruchen <zhiruchen1992@foxmail.com>
2018-08-05 17:35:18 +0800

    Merge sort

--- all/divideconquer/merge_sort_algorithm.go
+++ all/divideconquer/merge_sort_algorithm.go
@@ -1,7 +1,52 @@
 package divideconquer
 
 // MergeSortAlgorithm http://www.techiedelight.com/merge-sort/
-func MergeSortAlgorithm() {
-
+func MergeSortAlgorithm(array *[]int32) {
+	mergeSort(array, 0, len(*array)-1)
 }
 
+func mergeSort(arr *[]int32, low, high int) {
+	if high == low {
+		return
+	}
+
+	mid := low + (high-low)/2
+	mergeSort(arr, low, mid)
+	mergeSort(arr, mid+1, high)
+	mergeSubArrays(arr, low, mid, high)
+}
+
+func mergeSubArrays(arr *[]int32, low, mid, high int) {
+	k, i, j := 0, low, mid+1
+
+	var aux = make([]int32, (high - low + 1))
+
+	for i <= mid && j <= high {
+		if (*arr)[i] < (*arr)[j] {
+			aux[k] = (*arr)[i]
+			i++
+		} else {
+			aux[k] = (*arr)[j]
+			j++
+		}
+		k++
+	}
+
+	for i <= mid {
+		aux[k] = (*arr)[i]
+		k++
+		i++
+	}
+
+	for j <= high {
+		aux[k] = (*arr)[j]
+		k++
+		j++
+	}
+
+	var x = 0
+	for i := low; i <= high; i++ {
+		(*arr)[i] = aux[x]
+		x++
+	}
+}
--- all/divideconquer/merge_sort_algorithm_test.go
+++ all/divideconquer/merge_sort_algorithm_test.go
@@ -1,10 +1,25 @@
 package divideconquer
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestMergeSortAlgorithm(t *testing.T) {
-	
-}
+	cases := []struct {
+		array  []int32
+		result []int32
+	}{
+		{
+			array:  []int32{1, 3, 6, 5, 9, 2},
+			result: []int32{1, 2, 3, 5, 6, 9},
+		},
+	}
 
+	for _, c := range cases {
+		MergeSortAlgorithm(&c.array)
+		if !reflect.DeepEqual(c.array, c.result) {
+			t.Errorf("expect: %v, get: %v\n", c.result, c.array)
+		}
+	}
+}
