950c9a5891f86c8f01f5d6e27ae2abe7668bfb20
zhiruchen <zhiruchen1992@foxmail.com>
2018-08-05 11:28:57 +0800

    Add find_floor_and_ceil

--- all/divideconquer/find_floor_and_ceil_of_a_number_in_a_sorted_array.go
+++ all/divideconquer/find_floor_and_ceil_of_a_number_in_a_sorted_array.go
@@ -1,7 +1,60 @@
 package divideconquer
 
 // FindFloorAndCeilOfANumberInASortedArray http://www.techiedelight.com/find-floor-ceil-number-sorted-array/
-func FindFloorAndCeilOfANumberInASortedArray() {
-
+// 在一个排序数组里面寻找x的floor和ceil
+// floor: 数组里面小于等于x的最大的数, ceil: 数组里面大于等于x的最小的数
+func FindFloorAndCeilOfANumberInASortedArray(array []int32, x int32) (floor, ceil int32) {
+	low := 0
+	high := len(array) - 1
+	return getCeil(array, low, high, x), getFloor(array, low, high, x)
 }
 
+func getCeil(array []int32, low, high int, x int32) int32 {
+	for low <= high {
+		if array[low] >= x {
+			return array[low]
+		}
+
+		if array[high] < x {
+			return -1
+		}
+
+		mid := low + (high-low)/2
+		if array[mid] == x {
+			return array[mid]
+		} else if array[mid] > x {
+			high = mid
+		} else {
+			low = mid + 1
+		}
+	}
+
+	return -1
+}
+
+func getFloor(array []int32, low, high int, x int32) int32 {
+	for low <= high {
+		if x < array[low] {
+			return -1
+		}
+
+		if x >= array[high] {
+			return array[high]
+		}
+
+		mid := low + (high-low)/2
+		if low == mid {
+			return array[low]
+		}
+
+		if array[mid] == x {
+			return array[mid]
+		} else if array[mid] < x {
+			low = mid
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return -1
+}
--- all/divideconquer/find_floor_and_ceil_of_a_number_in_a_sorted_array_test.go
+++ all/divideconquer/find_floor_and_ceil_of_a_number_in_a_sorted_array_test.go
@@ -5,6 +5,48 @@ import (
 )
 
 func TestFindFloorAndCeilOfANumberInASortedArray(t *testing.T) {
-	
-}
+	cases := []struct {
+		array []int32
+		val   int32
+		ceil  int32
+		floor int32
+	}{
+		{
+			array: []int32{1, 4, 6, 8, 9},
+			val:   0,
+			ceil:  1,
+			floor: -1,
+		},
+		{
+			array: []int32{1, 4, 6, 8, 9},
+			val:   1,
+			ceil:  1,
+			floor: 1,
+		},
+		{
+			array: []int32{1, 4, 6, 8, 9},
+			val:   2,
+			ceil:  4,
+			floor: 1,
+		},
+		{
+			array: []int32{1, 4, 6, 8, 9},
+			val:   4,
+			ceil:  4,
+			floor: 4,
+		},
+		{
+			array: []int32{1, 4, 6, 8, 9},
+			val:   5,
+			ceil:  6,
+			floor: 4,
+		},
+	}
 
+	for _, c := range cases {
+		ceil, floor := FindFloorAndCeilOfANumberInASortedArray(c.array, c.val)
+		if ceil != c.ceil || c.floor != floor {
+			t.Errorf("expect ceil: %d, get: %d; expect floor: %d, get: %d\n", c.ceil, ceil, c.floor, floor)
+		}
+	}
+}
