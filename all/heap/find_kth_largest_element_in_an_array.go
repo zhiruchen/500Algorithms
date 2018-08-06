a1a73483d38cbd1534ef0ffcc80ef18afeb5a929
zhiruchen <zhiruchen1992@foxmail.com>
2018-08-06 23:53:38 +0800

    Find Kth Largest Number

--- all/heap/find_kth_largest_element_in_an_array.go
+++ all/heap/find_kth_largest_element_in_an_array.go
@@ -1,7 +1,21 @@
 package heap
 
+import (
+	"github.com/zhiruchen/500Algorithms/types"
+)
+
 // FindKthLargestElementInAnArray http://www.techiedelight.com/find-kth-largest-element-array/
-func FindKthLargestElementInAnArray() {
+// Find K’th largest element in an array
+func FindKthLargestElementInAnArray(array []int32, k int) int32 {
+	if len(array) == 1 {
+		return array[0]
+	}
 
+	h := types.NewHeap(array)
+
+	for i := 1; i <= k-1; i++ {
+		h.Pop()
+	}
+
+	return h.Pop()
 }
-
--- all/heap/find_kth_largest_element_in_an_array_test.go
+++ all/heap/find_kth_largest_element_in_an_array_test.go
@@ -5,6 +5,32 @@ import (
 )
 
 func TestFindKthLargestElementInAnArray(t *testing.T) {
-	
-}
+	cases := []struct {
+		array []int32
+		k     int
+		val   int32
+	}{
+		{
+			array: []int32{1, 3, 2, 6, 5, 9, 10, 0},
+			k:     5,
+			val:   3,
+		},
+		{
+			array: []int32{1, 3, 2, 6, 5, 9, 10, 0},
+			k:     1,
+			val:   10,
+		},
+		{
+			array: []int32{1, 3, 2, 6, 5, 9, 10, 0},
+			k:     3,
+			val:   6,
+		},
+	}
 
+	for _, c := range cases {
+		val := FindKthLargestElementInAnArray(c.array, c.k)
+		if val != c.val {
+			t.Errorf("Expect: %d. get: %d\n", c.val, val)
+		}
+	}
+}
