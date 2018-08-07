b901d2c7393de1ea3b9a23e13293be6064703057
zhiruchen <zhiruchen1992@foxmail.com>
2018-08-07 23:18:15 +0800

    Check Array is min heap

--- all/heap/check_if_given_array_represents_min_heap_or_not.go
+++ all/heap/check_if_given_array_represents_min_heap_or_not.go
@@ -1,7 +1,23 @@
 package heap
 
 // CheckIfGivenArrayRepresentsMinHeapOrNot http://www.techiedelight.com/check-given-array-represents-min-heap-not/
-func CheckIfGivenArrayRepresentsMinHeapOrNot() {
-
+func CheckIfGivenArrayRepresentsMinHeapOrNot(array []int32) bool {
+	newArray := []int32{-1}
+	newArray = append(newArray, array...)
+	return checkMinHeap(newArray, 1)
 }
 
+func checkMinHeap(array []int32, i int) bool {
+	// 如果 i 是一个叶子节点，返回true
+	if 2*i+1 > len(array) {
+		return true
+	}
+
+	var left, right bool
+	left = array[i] <= array[2*i] && checkMinHeap(array, 2*i)
+
+	right = 2*i+1 == len(array)
+	right = right || (array[i] <= array[2*i+1] && checkMinHeap(array, 2*i+1))
+
+	return left && right
+}
--- all/heap/check_if_given_array_represents_min_heap_or_not_test.go
+++ all/heap/check_if_given_array_represents_min_heap_or_not_test.go
@@ -5,6 +5,24 @@ import (
 )
 
 func TestCheckIfGivenArrayRepresentsMinHeapOrNot(t *testing.T) {
-	
-}
+	cases := []struct {
+		array     []int32
+		isMinHeap bool
+	}{
+		{
+			array:     []int32{1, 3, 5, 9, 8, 6, 7},
+			isMinHeap: true,
+		},
+		{
+			array:     []int32{1, 3, 5, 9, 8, 2, 0},
+			isMinHeap: false,
+		},
+	}
 
+	for _, c := range cases {
+		isMinHeap := CheckIfGivenArrayRepresentsMinHeapOrNot(c.array)
+		if isMinHeap != c.isMinHeap {
+			t.Errorf("expect %v, get: %v\n", c.isMinHeap, isMinHeap)
+		}
+	}
+}
