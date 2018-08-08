ff835107799695928a1566b444712df5d5d9eaa5
zhiruchen <zhiruchen1992@foxmail.com>
2018-08-08 20:31:53 +0800

    Reverse linkedlist

--- all/linkedlist/reverse_linked_list___part_1__iterative_solution.go
+++ all/linkedlist/reverse_linked_list___part_1__iterative_solution.go
@@ -1,7 +1,19 @@
 package linkedlist
 
+import (
+	"github.com/zhiruchen/500Algorithms/types"
+)
+
 // ReverseLinkedListPart1IterativeSolution http://www.techiedelight.com/reverse-linked-list-part-1-iterative-solution/
-func ReverseLinkedListPart1IterativeSolution() {
+func ReverseLinkedListPart1IterativeSolution(head *types.LinkedListNode) *types.LinkedListNode {
+	var cur *types.LinkedListNode
 
+	for head != nil {
+		var tmp *types.LinkedListNode
+		tmp, head.Next = head.Next, cur
+		cur = head
+		head = tmp
+	}
+
+	return cur
 }
-
--- all/linkedlist/reverse_linked_list___part_1__iterative_solution_test.go
+++ all/linkedlist/reverse_linked_list___part_1__iterative_solution_test.go
@@ -1,10 +1,40 @@
 package linkedlist
 
 import (
+	"reflect"
 	"testing"
+
+	"github.com/zhiruchen/500Algorithms/types"
 )
 
 func TestReverseLinkedListPart1IterativeSolution(t *testing.T) {
-	
-}
+	list := &types.LinkedList{
+		Head: &types.LinkedListNode{
+			Data: 10,
+			Next: &types.LinkedListNode{
+				Data: 9,
+				Next: &types.LinkedListNode{
+					Data: 8,
+					Next: &types.LinkedListNode{
+						Data: 7,
+						Next: nil,
+					},
+				},
+			},
+		},
+	}
 
+	newHead := ReverseLinkedListPart1IterativeSolution(list.Head)
+
+	var es []int32
+	head := newHead
+	for head != nil {
+		es = append(es, head.Data)
+		head = head.Next
+	}
+
+	expect := []int32{7, 8, 9, 10}
+	if !reflect.DeepEqual(expect, es) {
+		t.Errorf("expect %v. get: %v\n", expect, es)
+	}
+}
