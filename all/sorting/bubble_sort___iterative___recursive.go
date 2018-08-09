d65874eb922b9f7775b3864e058a34c21dbbde48
zhiruchen <zhiruchen1992@foxmail.com>
2018-08-09 17:18:04 +0800

    Bubble Sort

--- all/sorting/bubble_sort___iterative___recursive.go
+++ all/sorting/bubble_sort___iterative___recursive.go
@@ -1,7 +1,19 @@
 package sorting
 
 // BubbleSortIterativeRecursive http://www.techiedelight.com/bubble-sort-iterative-recursive/
-func BubbleSortIterativeRecursive() {
+func BubbleSortIterativeRecursive(array []int32) []int32 {
+	arrLen := len(array)
 
+	// 外层循环需要跑length-1次，因为当n-1个元素冒到正确的位置的时候，最后一个也到了正确的位置上
+	for i := 0; i < arrLen-1; i++ {
+
+		// n 个元素交换次数n-1次, 并且排除已完成排序的i个元素
+		for j := 0; j < arrLen-1-i; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+			}
+		}
+	}
+
+	return array
 }
-
--- all/sorting/bubble_sort___iterative___recursive_test.go
+++ all/sorting/bubble_sort___iterative___recursive_test.go
@@ -1,10 +1,25 @@
 package sorting
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestBubbleSortIterativeRecursive(t *testing.T) {
-	
-}
+	cases := []struct {
+		array  []int32
+		result []int32
+	}{
+		{
+			array:  []int32{1, 3, 5, 8, 7, 6},
+			result: []int32{1, 3, 5, 6, 7, 8},
+		},
+	}
 
+	for _, c := range cases {
+		result := BubbleSortIterativeRecursive(c.array)
+		if !reflect.DeepEqual(result, c.result) {
+			t.Errorf("expect: %v, get: %v\n", c.result, result)
+		}
+	}
+}
