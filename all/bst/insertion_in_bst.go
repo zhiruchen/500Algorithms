973e84e85820e1f55c68f6d190ebfc7370c05c97
zhiruchen <zhiruchen1992@foxmail.com>
2018-08-15 17:44:37 +0800

    Add search & insert of bst

--- all/bst/insertion_in_bst.go
+++ all/bst/insertion_in_bst.go
@@ -1,7 +1,20 @@
 package bst
 
+import (
+	"github.com/zhiruchen/500Algorithms/types"
+)
+
 // InsertionInBST http://www.techiedelight.com/insertion-in-bst/
-func InsertionInBST() {
+func InsertionInBST(node *types.BinTreeNode, key int32) *types.BinTreeNode {
+	if node == nil {
+		return &types.BinTreeNode{Data: key}
+	}
 
+	if key < node.Data {
+		node.Left = InsertionInBST(node.Left, key)
+	} else {
+		node.Right = InsertionInBST(node.Right, key)
+	}
+
+	return node
 }
-
--- all/bst/search_given_key_in_bst.go
+++ all/bst/search_given_key_in_bst.go
@@ -1,7 +1,22 @@
 package bst
 
+import (
+	"github.com/zhiruchen/500Algorithms/types"
+)
+
 // SearchGivenKeyInBST http://www.techiedelight.com/search-given-key-in-bst/
-func SearchGivenKeyInBST() {
+func SearchGivenKeyInBST(node *types.BinTreeNode, key int32) *types.BinTreeNode {
+	if node == nil {
+		return nil
+	}
 
+	if node.Data == key {
+		return node
+	}
+
+	if key < node.Data {
+		return SearchGivenKeyInBST(node.Left, key)
+	}
+
+	return SearchGivenKeyInBST(node.Right, key)
 }
-
