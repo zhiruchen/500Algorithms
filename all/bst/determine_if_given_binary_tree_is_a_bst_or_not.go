e70ff8d980a1f5adf3b24ee08457ad775db76a55
zhiruchen <zhiruchen1992@foxmail.com>
2018-08-16 10:39:16 +0800

    Add DetermineIfGivenBinaryTreeIsABSTOrNot

--- all/bst/determine_if_given_binary_tree_is_a_bst_or_not.go
+++ all/bst/determine_if_given_binary_tree_is_a_bst_or_not.go
@@ -1,7 +1,24 @@
 package bst
 
-// DetermineIfGivenBinaryTreeIsABSTOrNot http://www.techiedelight.com/determine-given-binary-tree-is-a-bst-or-not/
-func DetermineIfGivenBinaryTreeIsABSTOrNot() {
+import (
+	"math"
 
+	"github.com/zhiruchen/500Algorithms/types"
+)
+
+// DetermineIfGivenBinaryTreeIsABSTOrNot http://www.techiedelight.com/determine-given-binary-tree-is-a-bst-or-not/
+func DetermineIfGivenBinaryTreeIsABSTOrNot(node *types.BinTreeNode) bool {
+	return isBST(node, math.MinInt32, math.MaxInt32)
 }
 
+func isBST(node *types.BinTreeNode, minVal, maxVal int32) bool {
+	if node == nil {
+		return true
+	}
+
+	if node.Data < minVal || node.Data > maxVal {
+		return false
+	}
+
+	return isBST(node.Left, minVal, node.Data) && isBST(node.Right, node.Data, maxVal)
+}
--- all/bst/determine_if_given_binary_tree_is_a_bst_or_not_test.go
+++ all/bst/determine_if_given_binary_tree_is_a_bst_or_not_test.go
@@ -2,9 +2,57 @@ package bst
 
 import (
 	"testing"
+
+	. "github.com/zhiruchen/500Algorithms/types"
 )
 
 func TestDetermineIfGivenBinaryTreeIsABSTOrNot(t *testing.T) {
-	
-}
+	cases := []struct {
+		tree  *BinTreeNode
+		isBST bool
+	}{
+		{
+			tree: &BinTreeNode{
+				Data: 20,
+				Left: &BinTreeNode{
+					Data: 10,
+				},
+				Right: &BinTreeNode{
+					Data: 30,
+					Left: &BinTreeNode{
+						Data: 5,
+					},
+					Right: &BinTreeNode{
+						Data: 40,
+					},
+				},
+			},
+			isBST: false,
+		},
+		{
+			tree: &BinTreeNode{
+				Data: 20,
+				Left: &BinTreeNode{
+					Data: 10,
+				},
+				Right: &BinTreeNode{
+					Data: 30,
+					Left: &BinTreeNode{
+						Data: 25,
+					},
+					Right: &BinTreeNode{
+						Data: 40,
+					},
+				},
+			},
+			isBST: true,
+		},
+	}
 
+	for _, c := range cases {
+		result := DetermineIfGivenBinaryTreeIsABSTOrNot(c.tree)
+		if result != c.isBST {
+			t.Errorf("expect: %t, get: %t\n", c.isBST, result)
+		}
+	}
+}
