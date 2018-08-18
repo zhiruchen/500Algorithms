aacf6bc4c633e9b2eaabd9ac1900a492738f58a5
zhiruchen <zhiruchen1992@foxmail.com>
2018-08-18 09:09:57 +0800

    Add find floor ceil in bst

--- all/bst/floor_and_ceil_in_a_binary_search_tree.go
+++ all/bst/floor_and_ceil_in_a_binary_search_tree.go
@@ -1,7 +1,38 @@
 package bst
 
-// FloorAndCeilInABinarySearchTree http://www.techiedelight.com/floor-ceil-bst-iterative-recursive/
-func FloorAndCeilInABinarySearchTree() {
+import (
+	"math"
+	"github.com/zhiruchen/500Algorithms/types"
+)
 
+// FloorAndCeilInABinarySearchTree http://www.techiedelight.com/floor-ceil-bst-iterative-recursive/
+func FloorAndCeilInABinarySearchTree(root *types.BinTreeNode, key int32) (int32, int32) {
+	var min,max int32 = math.MinInt32, math.MaxInt32
+	var floor, ceil = &min, &max
+	findFloorAndCeilRecurInBST(root, floor, ceil, key)
+	return *floor, *ceil
 }
 
+func findFloorAndCeilRecurInBST(root *types.BinTreeNode, floor, ceil *int32, key int32) {
+	if root == nil {
+		return
+	}
+
+	// 如果找到了这个key, 那么floor和ceil都是这个key
+	if key == root.Data {
+		*ceil = root.Data
+		*floor = root.Data
+		return
+	}
+
+	// 如果key小于root，则在左子树寻找floor
+	if key < root.Data {
+		*ceil = root.Data
+		findFloorAndCeilRecurInBST(root.Left, floor, ceil, key)
+		return
+	}
+
+	// 如果key大于root, 则在右子树寻找ceil
+	*floor = root.Data
+	findFloorAndCeilRecurInBST(root.Right, floor, ceil, key)
+}
--- all/bst/floor_and_ceil_in_a_binary_search_tree_test.go
+++ all/bst/floor_and_ceil_in_a_binary_search_tree_test.go
@@ -1,10 +1,44 @@
 package bst
 
 import (
+	"math"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/zhiruchen/500Algorithms/types"
 )
 
 func TestFloorAndCeilInABinarySearchTree(t *testing.T) {
-	
-}
+	ast := assert.New(t)
 
+	cases := []struct {
+		elements    []int32
+		key         int32
+		floor, ceil int32
+	}{
+		{
+			elements: []int32{2, 4, 6, 8, 9, 10, 12},
+			key:      1,
+			floor:    math.MinInt32,
+			ceil:     2,
+		},
+		{
+			elements: []int32{2, 4, 6, 8, 9, 10, 12},
+			key:      3,
+			floor:    2,
+			ceil:     4,
+		},
+	}
+
+	for _, c := range cases {
+		bst := &types.BinarySearchTree{}
+		for _, v := range c.elements {
+			bst.Insert(v)
+		}
+
+		floor, ceil := FloorAndCeilInABinarySearchTree(bst.Root, c.key)
+		ast.Equal(c.floor, floor, "floor not equal")
+		ast.Equal(c.ceil, ceil, "ceil not equal")
+	}
+}
