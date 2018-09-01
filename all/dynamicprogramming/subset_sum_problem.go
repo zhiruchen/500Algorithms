96bc3bc7d619568738c9fcbf0c4695fe745d857f
zhiruchen <zhiruchen1992@foxmail.com>
2018-09-01 22:44:49 +0800

    DP: subset_sum

--- all/dynamicprogramming/subset_sum_problem.go
+++ all/dynamicprogramming/subset_sum_problem.go
@@ -1,7 +1,45 @@
 package dynamicprogramming
 
 // SubsetSumProblem http://www.techiedelight.com/subset-sum-problem/
-func SubsetSumProblem() {
+func SubsetSumProblem(array []int32, sum int32) ([]int32, bool) {
+	set := &[]int32{}
+	r := findSumSet(array, sum, len(array)-1, set)
+	if r {
+		return *set, true
+	}
 
+	return nil, false
 }
 
+func findSumSet(arr []int32, sum int32, n int, set *[]int32) bool {
+	// base case
+	if sum == 0 {
+		return true
+	}
+
+	if n < 0 || sum < 0 {
+		return false
+	}
+
+	// 包含当前item
+	*set = append(*set, arr[n])
+	include := findSumSet(arr, sum-arr[n], n-1, set)
+	if include {
+		return true
+	}
+
+	// 不包含item
+	removeFromSlice(set, arr[n])
+	exclude := findSumSet(arr, sum, n-1, set)
+	return exclude
+}
+
+func removeFromSlice(arr *[]int32, val int32) {
+	for i, v := range *arr {
+		if v == val {
+			left := (*arr)[i+1:]
+			*arr = (*arr)[0:i]
+			*arr = append(*arr, left...)
+		}
+	}
+}
--- all/dynamicprogramming/subset_sum_problem_test.go
+++ all/dynamicprogramming/subset_sum_problem_test.go
@@ -2,9 +2,40 @@ package dynamicprogramming
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSubsetSumProblem(t *testing.T) {
-	
+	arr := []int32{7, 3, 2, 5, 8}
+	var sum int32 = 14
+
+	set, has := SubsetSumProblem(arr, sum)
+	assert.True(t, has)
+	assert.True(t, arrayEqual([]int32{7, 2, 5}, set))
 }
 
+func TestRemoveFromSlice(t *testing.T) {
+	vals := []int32{1, 2, 3, 5, 6}
+	removeFromSlice(&vals, 3)
+	assert.Equal(t, []int32{1, 2, 5, 6}, vals)
+}
+
+func arrayEqual(a1, a2 []int32) bool {
+	if len(a1) != len(a2) {
+		return false
+	}
+
+Loop:
+	for _, v := range a1 {
+		for _, v1 := range a2 {
+			if v == v1 {
+				continue Loop
+			}
+		}
+
+		return false
+	}
+
+	return true
+}
