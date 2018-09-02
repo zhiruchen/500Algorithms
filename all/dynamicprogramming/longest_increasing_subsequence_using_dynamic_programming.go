37d6dad81d7a177b1178f6bc41606011221d8778
zhiruchen <zhiruchen1992@foxmail.com>
2018-09-02 12:07:19 +0800

    DP: Longest Increasing SubSeq

--- all/dynamicprogramming/longest_increasing_subsequence_using_dynamic_programming.go
+++ all/dynamicprogramming/longest_increasing_subsequence_using_dynamic_programming.go
@@ -1,7 +1,46 @@
 package dynamicprogramming
 
-// LongestIncreasingSubsequenceUsingDynamicProgramming http://www.techiedelight.com/longest-increasing-subsequence-using-dynamic-programming/
-func LongestIncreasingSubsequenceUsingDynamicProgramming() {
+import (
+	"math"
 
+	"github.com/zhiruchen/500Algorithms/common"
+)
+
+// LongestIncreasingSubsequenceUsingDynamicProgramming http://www.techiedelight.com/longest-increasing-subsequence-using-dynamic-programming/
+func LongestIncreasingSubsequenceUsingDynamicProgramming(seq []int32) int {
+	return longestIncreasingSubsequence(seq, 0, len(seq), math.MinInt32)
 }
 
+func longestIncreasingSubsequence(seq []int32, i, n int, prev int32) int {
+	if n == i {
+		return 0
+	}
+
+	// exclude
+	excluLcsLen := longestIncreasingSubsequence(seq, i+1, n, prev)
+
+	// include current item
+	var includeLCSLen = 0
+	if seq[i] > prev {
+		includeLCSLen = 1 + longestIncreasingSubsequence(seq, i+1, n, seq[i])
+	}
+
+	return common.IntMax(includeLCSLen, excluLcsLen)
+}
+
+func LongestIncreasingSubsequenceIter(seq []int32) int {
+	lenTable := make([]int, len(seq))
+	for i := range lenTable {
+		lenTable[i] = 1
+	}
+
+	for i := 1; i < len(seq); i++ {
+		for j := 0; j < i; j++ {
+			if seq[j] <= seq[i] {
+				lenTable[i] = common.IntMax(lenTable[i], lenTable[j]+1)
+			}
+		}
+	}
+
+	return lenTable[len(seq)-1]
+}
--- all/dynamicprogramming/longest_increasing_subsequence_using_dynamic_programming_test.go
+++ all/dynamicprogramming/longest_increasing_subsequence_using_dynamic_programming_test.go
@@ -2,9 +2,48 @@ package dynamicprogramming
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLongestIncreasingSubsequenceUsingDynamicProgramming(t *testing.T) {
-	
+	cases := []struct {
+		seq    []int32
+		length int
+	}{
+		{
+			seq:    []int32{1, 0, 2, 3, 6, 5},
+			length: 4, // 1,2,3,6 | 0,2,3,6
+		},
+		{
+			seq:    []int32{0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15},
+			length: 6, // 0, 2, 6, 9, 11, 15
+		},
+	}
+
+	for _, c := range cases {
+		length := LongestIncreasingSubsequenceUsingDynamicProgramming(c.seq)
+		assert.Equal(t, c.length, length)
+	}
 }
 
+func TestLongestIncreasingSubsequenceIter(t *testing.T) {
+	cases := []struct {
+		seq    []int32
+		length int
+	}{
+		{
+			seq:    []int32{1, 0, 2, 3, 6, 5},
+			length: 4, // 1,2,3,6 | 0,2,3,6
+		},
+		{
+			seq:    []int32{0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15},
+			length: 6, // 0, 2, 6, 9, 11, 15
+		},
+	}
+
+	for _, c := range cases {
+		length := LongestIncreasingSubsequenceIter(c.seq)
+		assert.Equal(t, c.length, length)
+	}
+}
