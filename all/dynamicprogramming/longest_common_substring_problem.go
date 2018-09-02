92b5788362937bef38c45b2f435c79c07affe0fa
zhiruchen <zhiruchen1992@foxmail.com>
2018-09-02 23:26:00 +0800

    DP: Longest Common SubString

--- all/dynamicprogramming/longest_common_substring_problem.go
+++ all/dynamicprogramming/longest_common_substring_problem.go
@@ -1,7 +1,31 @@
 package dynamicprogramming
 
 // LongestCommonSubstringProblem http://www.techiedelight.com/longest-common-substring-problem/
-func LongestCommonSubstringProblem() {
+func LongestCommonSubstringProblem(a, b string) int {
+	lengthTable := make([][]int, len(a))
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(b); j++ {
+			lengthTable[i] = append(lengthTable[i], 0)
+		}
+	}
 
+	var l = 0
+
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(b); j++ {
+			if a[i] == b[j] {
+				if i >= 1 && j >= 1 {
+					lengthTable[i][j] = lengthTable[i-1][j-1] + 1
+				} else {
+					lengthTable[i][j] = 1
+				}
+
+				if lengthTable[i][j] > l {
+					l = lengthTable[i][j]
+				}
+			}
+		}
+	}
+
+	return l
 }
-
--- all/dynamicprogramming/longest_common_substring_problem_test.go
+++ all/dynamicprogramming/longest_common_substring_problem_test.go
@@ -2,9 +2,30 @@ package dynamicprogramming
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLongestCommonSubstringProblem(t *testing.T) {
-	
-}
+	cases := []struct {
+		s1  string
+		s2  string
+		lcs int
+	}{
+		{
+			s1:  "BAC",
+			s2:  "BABA",
+			lcs: 2,
+		},
+		{
+			s1:  "ABABC",
+			s2:  "BABCA",
+			lcs: 4,
+		},
+	}
 
+	for _, c := range cases {
+		len := LongestCommonSubstringProblem(c.s1, c.s2)
+		assert.Equal(t, c.lcs, len)
+	}
+}
