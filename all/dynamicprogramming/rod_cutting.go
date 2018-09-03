114a0e2c0ac9e6ec26d7262dbad905ba66373b2f
zhiruchen <zhiruchen1992@foxmail.com>
2018-09-03 23:38:27 +0800

    DP: Rod Cutting

--- all/dynamicprogramming/rod_cutting.go
+++ all/dynamicprogramming/rod_cutting.go
@@ -1,7 +1,38 @@
 package dynamicprogramming
 
-// RodCutting http://www.techiedelight.com/rot-cutting/
-func RodCutting() {
+import (
+	"math"
 
+	"github.com/zhiruchen/500Algorithms/common"
+)
+
+// RodCutting http://www.techiedelight.com/rot-cutting/
+func RodCutting(price []int32, rodLen int) int32 {
+	if rodLen == 0 {
+		return 0
+	}
+
+	var maxValue int32 = math.MinInt32
+	for i := 1; i <= rodLen; i++ {
+		profit := price[i-1] + RodCutting(price, rodLen-i)
+
+		if profit > maxValue {
+			maxValue = profit
+		}
+	}
+
+	return maxValue
 }
 
+func RodCuttingIter(price []int32, rodLen int) int32 {
+	t := make([]int32, rodLen+1)
+
+	for i := 1; i <= rodLen; i++ {
+		//
+		for j := 1; j <= i; j++ {
+			t[i] = common.Max(t[i], price[j-1]+t[i-j])
+		}
+	}
+
+	return t[rodLen]
+}
--- all/dynamicprogramming/rod_cutting_test.go
+++ all/dynamicprogramming/rod_cutting_test.go
@@ -2,9 +2,22 @@ package dynamicprogramming
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRodCutting(t *testing.T) {
-	
+	price := []int32{1, 5, 8, 9, 10, 17, 17, 20}
+	rodLen := 4
+
+	maxVal := RodCutting(price, rodLen)
+	assert.Equal(t, int32(10), maxVal)
 }
 
+func TestRodCuttingIter(t *testing.T) {
+	price := []int32{1, 5, 8, 9, 10, 17, 17, 20}
+	rodLen := 4
+
+	maxVal := RodCuttingIter(price, rodLen)
+	assert.Equal(t, int32(10), maxVal)
+}
